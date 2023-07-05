7d77b740137defd7006abab75dddff0ba2f9417b
asadalimcj <120159273+asadalimcj@users.noreply.github.com>
2023-07-06 00:06:47 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace GUIgame
+{
+    partial class game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = global::GUIgame.Properties.Resources.player;
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player.Location = new System.Drawing.Point(150, 388);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(51, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GUIgame.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "game";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EZInput;
+
+namespace GUIgame
+{
+    public partial class game : Form
+    {
+        public List<PictureBox> fireBullet = new List<PictureBox>();
+        PictureBox enemy = new PictureBox();
+        string enemyDirection = "MovingRight";
+        public game()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            enemy = makeEnemy(Properties.Resources.monster);
+            this.Controls.Add(enemy);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enemy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(Keyboard.IsKeyPressed(Key.RightArrow))
+            {
+                player.Left += 25;
+            }
+            if(Keyboard.IsKeyPressed(Key.LeftArrow))
+            {
+                player.Left -= 25;
+            }
+            if(Keyboard.IsKeyPressed(Key.Space))
+            {
+                Image img = Properties.Resources.fire;
+                PictureBox fire = createFire(img, player);
+                fireBullet.Add(fire);
+                this.Controls.Add(fire);
+            }
+            foreach(PictureBox bullet in fireBullet)
+            {
+                bullet.Top = bullet.Top - 20;
+            }
+            for(int idx = 0;idx < fireBullet.Count; idx++)
+            {
+                if(fireBullet[idx].Bottom < 0)
+                {
+                    fireBullet.Remove(fireBullet[idx]);
+                }
+            }
+            moveEnemy(enemy, ref enemyDirection);
+        }
+        Random rand = new Random();
+        private PictureBox makeEnemy(Image img)
+        {
+            PictureBox enemy = new PictureBox();
+            int left = rand.Next(30, this.Width);
+            int top = rand.Next(5, img.Height + 20);
+
+            enemy.Left = left;
+            enemy.Top = top;
+            enemy.Height = img.Height;
+            enemy.Width = img.Width;
+            enemy.BackColor = Color.Transparent;
+            enemy.Image = img;
+            return enemy;
+        }
+        private void moveEnemy(PictureBox enemy, ref string enemyDirection)
+        {
+            if(enemyDirection == "MovingRight")
+            {
+                enemy.Left = enemy.Left + 10;
+            }
+            if (enemyDirection == "MovingLeft")
+            {
+                enemy.Left = enemy.Left - 10;
+            }
+            if((enemy.Left + enemy.Width) > this.Width)
+            {
+                enemyDirection = "MovingLeft";
+            }
+            if(enemy.Left <= 2)
+            {
+                enemyDirection = "MovingRight";
+            }
+        }
+        private PictureBox createFire(Image fireImage, PictureBox source)
+        {
+            PictureBox fire = new PictureBox();
+            fire.Image = fireImage;
+            fire.Height = fireImage.Height;
+            fire.Width = fireImage.Width;
+            fire.BackColor = Color.Transparent;
+            System.Drawing.Point location;
+            location = new System.Drawing.Point();
+            location.X = source.Left + (source.Width / 2) - 5;
+            location.Y = source.Top;
+            fire.Location = location;
+            return fire;
+
+        }
+    }
+}
